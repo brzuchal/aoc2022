c6ee3b2d13c8e2d490877b567280b5c67bc941d9
Michał Brzuchalski <michal.brzuchalski@gmail.com>
2022-12-05 08:23:51 +0100

    Count pairs of elves who have the same sector in range

---
+++ aoc4b.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+$totalScore = 0;
+while (!feof(STDIN)) {
+    $line = fgetcsv(STDIN);
+    if (!$line) break;
+//    var_dump($line);
+    [$leftMin, $leftMax] = explode("-",$line[0]);
+    [$rightMin, $rightMax] = explode("-",$line[1]);
+//    var_dump($leftMin, $leftMax);
+//    var_dump($rightMin, $rightMax);
+
+    if (($rightMin >= $leftMin && $rightMin <= $leftMax) || ($rightMax >= $leftMin && $rightMax <= $leftMax)) {
+        $totalScore ++;
+        continue;
+    }
+
+    if (($leftMin >= $rightMin && $leftMin <= $rightMax) || ($leftMax >= $rightMin && $leftMax <= $rightMax)) {
+        $totalScore ++;
+    }
+}
+
+echo "Total score: {$totalScore}\n";
